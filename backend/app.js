854b655885123b02a542e41d2564c37955dd84b3
Agush <a_clemente@live.com.ar>
2025-02-01 22:34:11 -0300

    Agregado back y DB

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+ import cors from 'cors';
+ import comidasRouter from './routes/comidas.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/comidas', comidasRouter);
+
+export default app;
+
